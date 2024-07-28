afdc25ce8f9aa5b6fff68aea9aa4e951784dd914
Sarthak-G07 <22103271@mail.jiit.ac.in>
2024-07-28 20:10:52 +0530

    Add two cards

--- app/src/main/java/com/example/firstdemoapp/MainActivity.kt
+++ app/src/main/java/com/example/firstdemoapp/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.example.firstdemoapp
 
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.widget.Button
 import android.widget.LinearLayout
 import android.widget.Toast
+import androidx.cardview.widget.CardView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -33,5 +36,18 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
+        val webButton = findViewById<CardView>(R.id.cardWeb)
+        val cameraButton = findViewById<CardView>(R.id.cardCamera)
+
+        webButton.setOnClickListener {
+            val intent = Intent(Intent.ACTION_VIEW)
+            intent.data = Uri.parse("https://www.youtube.com/")
+            startActivity(intent)
+        }
+
+        cameraButton.setOnClickListener {
+            val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            startActivity(intent)
+        }
     }
 }
